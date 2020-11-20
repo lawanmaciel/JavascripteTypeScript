c7dd9973798307651c0fec83410bb8e517ac25ee
Lawan Maciel <irolawan33@gmail.com>
2020-11-20 15:59:00 -0300

    Subindo dados #3

---
+++ JsBasico/aula14.js
@@ -0,0 +1,10 @@
+let num1 = 10; // Number
+let num2 = 2; //Number
+
+//console.log(num1.toString() + num2); //Converter para string
+// num1 = num1.toString();
+// console.log(num1.toString(2)) // Conversão para binário
+// console.log(num1.toFixed(2)) // Arrendonda o valor (numero de casas decimais)
+// console.log(Number.isInteger(num1)) // Para saber se o número é inteiro
+// console.log(Number.isNaN(exemplo)) // Para saber se o número é um numero (retorna true or false)
+
---
+++ JsBasico/aula15.js
@@ -0,0 +1,8 @@
+let num1 = 9.903293;
+// let num2 = Math.floor(num1) // arredonda o número para baixo
+// let num2 = Math.ceil(num1) // arredonda o número para cima
+// let num2 = Math.round(num1) // arredonda o número mais próximo
+// let num2 = Math.round(num1) // arredonda o número mais próximo
+// console.log(Math.max(1,2,3,4)) // Ele exibe o maior número que exite
+// console.log(Math.min(1,2,3,4)) // Ele exibe o menor número que exite
+// console.log(Math.random()) // Gera um número aleatório
